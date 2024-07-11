425ddc497de716036ee9d24f6566bdeb0e2caa35
David Molnar <dmolnar@qualityunit.com>
2024-07-11 14:23:29 +0200

    add test case to check for incorrect quality upper bound refactoring

--- fixtures/texttest_fixture.php
+++ fixtures/texttest_fixture.php
@@ -15,6 +15,7 @@ $items = [
     new Item('Elixir of the Mongoose', 5, 7),
     new Item('Sulfuras, Hand of Ragnaros', 0, 80),
     new Item('Sulfuras, Hand of Ragnaros', -1, 80),
+    new Item('Backstage passes to a TAFKAL80ETC concert', 17, 28),
     new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20),
     new Item('Backstage passes to a TAFKAL80ETC concert', 10, 49),
     new Item('Backstage passes to a TAFKAL80ETC concert', 5, 49),
