4ec312a932c272cb1b081dfb3867c94669cfc007
David Molnar <dmolnar@qualityunit.com>
2024-07-11 10:51:39 +0200

    intial commit

---
+++ tests/ApprovalTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use ApprovalTests\Approvals;
+
+/**
+ * This unit test uses [Approvals](https://github.com/approvals/ApprovalTests.php).
+ *
+ * There are two test cases here with different styles:
+ * <li>"foo" is more similar to the unit test from the 'Java' version
+ * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
+ *
+ * I suggest choosing one style to develop and deleting the other.
+ */
+class ApprovalTest extends TestCase
+{
+
+    public function testThirtyDays(): void
+    {
+        ob_start();
+
+        $argv = ["", "30"];
+        include(__DIR__ . '/../fixtures/texttest_fixture.php');
+
+        $output = ob_get_clean();
+
+        Approvals::verifyString($output);
+    }
+}
